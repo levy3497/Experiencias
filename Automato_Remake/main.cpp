3d9677c204a1cb78b048855b7715ae43ab8772c2
levy3497 <myhsaellevyd@gmail.com>
2026-04-03 11:30:21 -0300

    Automato_Remake Update!

--- Automato_Remake/main.cpp
+++ Automato_Remake/main.cpp
@@ -2,18 +2,40 @@
 #include <stdlib.h>
 #include "automato.hpp"
 #include "montagem.hpp"
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
 using namespace std;
 
-//arquivo main.cpp
+int main() {
+    ma_engine engine;
+    ma_engine_init(NULL, &engine);
+    ma_engine_play_sound(&engine, "audio_fundo.mp3", NULL);
 
-int main(){
     automato automato_main_player;
+    automato automato_main_ia;
 
     impressoes_iniciais();
 
     montar_automato_player(automato_main_player);
-    
-    automato_main_player.imprimir();
+    montar_automato_ia(automato_main_ia, automato_main_player.get_energia());
 
+    automato_main_player.imprimir();
+    automato_main_ia.imprimir();
+
+    cout << "\nPressione ENTER para encerrar...";
+    cin.ignore(); 
+    cin.get();
+
+    ma_engine_uninit(&engine);
     return 0;
 }
+
+/*
+--------------------------------------------------------------------------------
+AVISO DE AUTORIA:
+Todo o sistema de áudio e integração da biblioteca miniaudio.h foi projetado 
+e implementado via vibe coding com o modelo de IA Gemini da Google.
+--------------------------------------------------------------------------------
+*/
