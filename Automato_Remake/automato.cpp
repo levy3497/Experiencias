345079a5787344d94b399cf86dbd79994d3d1a43
levy3497 <myhsaellevyd@gmail.com>
2026-04-03 11:33:26 -0300

    Refactor automato methods for consistency and clarity

--- Automato_Remake/automato.cpp
+++ Automato_Remake/automato.cpp
@@ -4,20 +4,23 @@
 
 //Arquivo automato.cpp
 
-void automato::imprimir(){
+void automato::imprimir() {
+    std::cout << "#############################################\n";
     std::cout << "Seu nome e: " << nome << std::endl;
-    std::cout << "Total energia de: " <<  energia << std::endl;
+    std::cout << "Total energia de: " << energia << std::endl;
     std::cout << "Total de ataque de: " << ataque << std::endl;
     std::cout << "Total de defesa de: " << defesa << std::endl;
+    std::cout << "Sua classe e de: " << classe_nome << std::endl;
+    std::cout << "#############################################\n";
 }
 
-void impressoes_iniciais(){
+void impressoes_iniciais() {
     std::cout << "----------------\n";
     std::cout << "AUTOMATO REMAKE\n";
     std::cout << "----------------\n";
 }
 
-int gerar_num_aleatorio(int min, int max){
+int gerar_num_aleatorio(int min, int max) {
     int num_aleatorio;
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -26,26 +29,26 @@ int gerar_num_aleatorio(int min, int max){
     return num_aleatorio;
 }
 
-void automato::set_energia(int energia){
+void automato::set_energia(int energia) {
     this->energia = energia;
 }
 
-int automato::get_energia(){
+int automato::get_energia() {
     return energia;
 }
 
-void automato::set_ataque(int ataque){
+void automato::set_ataque(int ataque) {
     this->ataque = ataque;
 }
 
-int automato::get_ataque(){
+int automato::get_ataque() {
     return ataque;
 }
 
-void automato::set_defesa(int defesa){
+void automato::set_defesa(int defesa) {
     this->defesa = defesa;
 }
 
-int automato::get_defesa(){
+int automato::get_defesa() {
     return defesa;
 }
